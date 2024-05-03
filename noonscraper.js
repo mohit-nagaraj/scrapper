54ad09fa795d49efd32ae89d5f8aba9e7728996a
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-05-03 10:14:31 +0530

    working v2
    Signed-off-by: mohit-nagaraj <mohitnagaraj20@gmail.com>

--- noonscraper.js
+++ noonscraper.js
@@ -1,10 +1,9 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 async function scrapeWebsite(pageLimit) {
   console.log("Scraping website...");
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const csvWriter = createCsvWriter({
     path: "out.csv",
     header: [
@@ -12,48 +11,68 @@ async function scrapeWebsite(pageLimit) {
       { id: "desc", title: "Description" },
       { id: "cat", title: "Category" },
       { id: "ben", title: "Benefits" },
-      { id: "elg", title: "Eligibility" },
-      { id:"doc", title: "Documents"},
-      { id: "proc", title: "Process" },
+      { id: "eli", title: "Eligibility" },
+      { id: "proc", title: "Procedure" },
+      { id: "doc", title: "Documents" },
       { id: "link", title: "Link" },
     ],
   });
-  console.log("Browser opened...")
+  console.log("Browser opened...");
   const page = await browser.newPage();
-  await page.goto(
-    `https://www.myscheme.gov.in/search`,
-    { waitUntil: "networkidle0",timeout:0 }
-  );
-  
-  // const data = await page.$$eval("h2.tracking-wide.text-sm.text-darkIndigo-900.font-normal");
-  // console.log("data",data)
-  // console.log("Browser loaded...")
-  // for (let i = 1; i <= pageLimit; i++) {
-  //   console.log(`Scraping page ${i}...`);
-    
+  await page.goto(`https://www.myscheme.gov.in/search`, {
+    waitUntil: "networkidle0",
+    timeout: 0,
+  });
 
-  //   // const data = await page.$$eval("h2.tracking-wide.text-sm.text-darkIndigo-900.font-normal", (elements) =>
-  //   //   console.log("elements",elements)
-  //   //   // elements.map((item) => {
-  //   //   //   console.log("item",item);
-  //   //   //   return {
-  //   //   //     text: "acb",//item.querySelector(".jyQuMr").title,
-  //   //   //     link: "dec",//item.querySelector(".dvQhRS>a").href,
-  //   //   //     mrp: "wad",//item.querySelector(".oldPrice")?.textContent ?? "N/A",
-  //   //   //     sp: "wdawa",//item.querySelector(".hCDaLm>strong").textContent,
-  //   //   //   };
-  //   //   // })
-  //   // );
+  for (let i = 1; i <= pageLimit; i++) {
+    console.log(`Scraping page ${i}...`);
 
-  //   // Write the records to the CSV file
-  //   // await csvWriter.writeRecords(data);
-  //   // console.log("Page scraped...");
-  //   // await page.close();
-  // }
+    const data = await page.$$eval(
+      ".mx-auto.rounded-xl.shadow-md.overflow-hidden.w-full.group",
+      (elements) =>
+        elements.map((item) => {
+          const link = item.querySelector(
+            "a.undefined.block.mt-1.text-xl.leading-tight.font-semibold.text-secondary"
+          ).href;
+          return {
+            link: link,
+          };
+        })
+    );
+    const fulldata=[];
+    for (let j = 0; j < data.length; j++) {
+      console.log(`Visiting link ${j + 1}...`);
+      const link = data[j].link;
+      const newPage = await browser.newPage();
+      await newPage.goto(link, { waitUntil: "networkidle0", timeout: 0 });
+      // Perform actions on the individual page
+      const title = await newPage.$eval(
+        "h1.font-bold.text-green-600.text-xl",
+        (element) => element.innerText
+      );
+      const desc = await newPage.$$eval(
+        "#details > div> div> div ",
+        (elements) =>
+          elements.map((element) => element.innerText)
+      );
+      const pageData= {
+        title: title,
+        desc: desc[0],
+        link: data[j].link,
+      }
+      console.log(pageData);
+      fulldata.push(pageData);
+      await newPage.close();
+    }
+    // Write the records to the CSV file
+    await csvWriter.writeRecords(fulldata);
+    console.log("Page scraped...");
 
+    // Click the next button
+  }
 
   console.log("...Done");
   await browser.close();
 }
 
-scrapeWebsite(208);
+scrapeWebsite(1);
